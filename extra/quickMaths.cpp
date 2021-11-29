8c180bce688dda1e95860b0857292084d3d2d13f
darianodirling <darianodirling@gmail.com>
2021-11-29 09:44:44 -0800

    assignmeny

---
+++ extra/quickMaths.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double answer; 
+    int remainder, a;
+    remainder = 6 % 31;
+
+    cout << remainder  << endl;
+}
